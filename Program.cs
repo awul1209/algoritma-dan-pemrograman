eea1456d46f29589770ec58c76304e8f527b9dd6
awul1209 <awulawul819@gmail.com>
2026-08-30 19:37:53 +0700

    Add files via upload
    hello world

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+
+          //ini adalah contoh semantik
+            int angka1 = 2;
+            int angka2 = 3;
+
+            Console.WriteLine("hasil angka1 + angka2 = " + angka1 * angka2);
+
+            //ini adalah statement dan expresion
+            int angka = 4 + 2;
+            Console.WriteLine(angka);
+
+            Console.WriteLine("===== Biodata Saya=======");
+            Console.WriteLine("Nama : Ayyub");
+            Console.WriteLine("Hoby : Futsal & \n Bola");
+        }
+    }
+}
